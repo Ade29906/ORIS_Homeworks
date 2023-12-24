47901c7870eca2102f8a4f399fa16710c6291ef7
Ade29906 <154759236+Ade29906@users.noreply.github.com>
2023-12-25 00:43:43 +0300

    Add files via upload

---
+++ Homework5/MyHttpServer/Configuration/AppsettingsLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace MyHttpServer.Configuration;
+
+public class AppSettingsLoader
+{
+    public static AppSettings Init(string pathConfigFile)
+    {
+        if (!File.Exists(pathConfigFile))
+        { 
+            Console.WriteLine("Файл appsettings.json не был найден");
+            throw new Exception();
+        }
+
+        using var file = File.OpenRead(pathConfigFile);
+        var config = JsonSerializer.Deserialize<AppSettings>(file);
+        return config!;
+    }
+}
